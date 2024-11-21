853643c766d51779d1dafeabd0dd465578cc4370
AABelimov <belimov98@gmail.com>
2024-11-21 12:28:45 +0300

    initial commit

---
+++ src/main/java/ru/aabelimov/soimonitor/SoiMonitorApplication.java
@@ -0,0 +1,13 @@
+package ru.aabelimov.soimonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoiMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoiMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/aabelimov/soimonitor/SoiMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aabelimov.soimonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoiMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
